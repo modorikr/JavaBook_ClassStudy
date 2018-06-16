424e676a828f69e9f3b0f09bfc53c8d24d2fd43c
KangP <KangP@A206439.fremont.lamrc.net>
2018-06-15 23:45:17 -0700

    fbgfg

---
+++ JavaBook_ClassStudy/src/HelloOregon.java
@@ -0,0 +1,11 @@
+
+public class HelloOregon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Oregon!!! + \n");
+		System.out.println("Good Morning!!!");
+	}
+
+}
